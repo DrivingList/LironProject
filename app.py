aee7aac24b64823e98687efdb4d1e553a8cc481c
DrivingList <108739164+DrivingList@users.noreply.github.com>
2022-09-08 15:46:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<body style="background-color:Yellow;"><h1 style="color:Blue;">Ayra is fat and super gay</h1></body>'
+    return '<body style="background-color:Yellow;"><h1 style="color:Blue;">"Hello World"</h1></body>'
 
 app.run(host='0.0.0.0', port=8081)
